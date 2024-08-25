72283e89003bcfd1d548fbdae339c516051bde94
Raine <143972065+GabrielNakamoto@users.noreply.github.com>
2024-08-24 20:26:01 -0400

    Added custom bash commands file

---
+++ .customCommands.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+ function fpro(){
+     local LOCAL_PATH="$HOME/Desktop/Workspace"
+     cd "$LOCAL_PATH"
+     local selected=$(find . -maxdepth 2 -type d | fzf --tmux 80%,80% )
+     cd $OLDPWD
+     if [ "$selected" != "" ] && [ "$selected" != "." ]; then
+         # cd $selected
+         # echo "${LOCAL_PATH}${selected:1}"
+         $(tmux new-window -c "${LOCAL_PATH}${selected:1}")
+     fi
+ }
+
+
+function nf(){
+    local selected=$(fd . --type f --strip-cwd-prefix | fzf --tmux 80%,80% )
+    # local selected=$(fzf --tmux 80%,80% )
+    if [ "$selected" != "" ]; then
+        nvim $selected
+    fi
+}
+
+function fdir(){
+	cd $(fd -t d | fzf)
+}
+
+function tsesh(){
+    tmux new -s $1
+}
+
+function ntype(){
+    echo "#include "$1.h"" > $1.cpp
+    echo "#pragma once" > $1.h
+}
